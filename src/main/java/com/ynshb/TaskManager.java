9f0ba17fa785f51f4dd6e68d678efb37b5521682
Ynshb <youness.habach@gmail.om>
2025-12-07 20:57:13 +0100

    WEEK1 - day 4

--- src/main/java/com/ynshb/TaskManager.java
+++ src/main/java/com/ynshb/TaskManager.java
@@ -1,12 +1,22 @@
 package com.ynshb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManager {
+public class TaskManager implements CommandLineRunner {
+
+    @Autowired
+    YounessService younessService;
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManager.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        younessService.printUser();
     }
 }
